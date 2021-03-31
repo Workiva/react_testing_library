4c3b6031b614d95accb219426ac7133044e8a489
Aaron Lademann <aaron.lademann@workiva.com>
2021-03-31 09:51:10 -0700

    Remove unused import

--- test/unit/dom/fire_event_test.dart
+++ test/unit/dom/fire_event_test.dart
@@ -14,7 +14,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import 'dart:developer';
 import 'dart:html';
 
 import 'package:react/react.dart' as react;
