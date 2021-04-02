f66e26df9c567946b97a32ac38882e2f3348e1fb
Aaron Lademann <aaron.lademann@workiva.com>
2021-04-02 14:40:34 -0700

    Use public entrypoint in tests
    + As a sort of “test” that the matchers are public.

--- test/unit/dom/matchers/is_empty_dom_element_test.dart
+++ test/unit/dom/matchers/is_empty_dom_element_test.dart
@@ -15,7 +15,7 @@
 // limitations under the License.
 
 import 'package:react/react.dart' as react;
-import 'package:react_testing_library/react_testing_library.dart';
+import 'package:react_testing_library/react_testing_library.dart' show isEmptyDomElement, render;
 import 'package:react_testing_library/src/util/over_react_stubs.dart';
 import 'package:test/test.dart';
 
