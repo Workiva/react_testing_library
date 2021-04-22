dc4bb2dd948311d7faba85fa981031dabb0c75d2
Aaron Lademann <aaron.lademann@workiva.com>
2021-04-22 13:00:15 -0700

    Merge in entrypoint changes from initial release

---
+++ lib/matchers/matchers.dart
@@ -0,0 +1,25 @@
+// @dart = 2.7
+
+// Copyright 2021 Workiva Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/// {@category Dom}
+library rtl.matchers;
+
+export '../src/matchers/contains_element.dart' show containsElement;
+export '../src/matchers/css_class_matchers.dart' show hasClasses, hasExactClasses, excludesClasses;
+export '../src/matchers/is_disabled.dart' show isDisabled, isEnabled;
+export '../src/matchers/is_in_the_document.dart' show isInTheDocument;
+export '../src/matchers/is_empty_dom_element.dart' show isEmptyDomElement;
+export '../src/matchers/is_focused.dart' show isFocused;
