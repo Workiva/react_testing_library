343471ebd15c889536d076338bd85b2c1395c55e
Sydney Jodon <sydney.jodon@workiva.com>
2025-05-07 14:23:42 -0700

    Add test for new RTL issue to verify React 17 does not have it

--- test/unit/react_version_test.dart
+++ test/unit/react_version_test.dart
@@ -2,18 +2,29 @@
 library react_version_test;
 
 import 'package:js/js.dart';
+import 'package:react/react.dart' as react;
+import 'package:react_testing_library/matchers.dart';
+import 'package:react_testing_library/react_testing_library.dart';
 import 'package:test/test.dart';
 
 void main() {
   // These tests help us verify our test setup, ensuring
   // we're running on the React version we expect to be.
-  test('Setup check: window.React is React 17', () {
-    expect(reactVersion, startsWith('17.'));
-  }, tags: 'react-17');
+  group('Setup check: window.React is', () {
+    test('Setup check: window.React is React 17', () {
+      expect(reactVersion, startsWith('17.'));
+    }, tags: 'react-17');
 
-  test('Setup check: window.React is React 18', () {
-    expect(reactVersion, startsWith('18.'));
-  }, tags: 'react-18');
+    test('Setup check: window.React is React 18', () {
+      expect(reactVersion, startsWith('18.'));
+    }, tags: 'react-18');
+  });
+
+  // Verify that React 17 stays on a version before this issue: https://github.com/eps1lon/dom-accessibility-api/issues/1075
+  test('Verify that RTL version breaking change does not exist for React 17 version', () {
+    final view = render(react.button({}, ['abc', react.span({}, ' - '), 'abc']));
+    expect(view.getByRole('button', name: 'abc - abc'), isInTheDocument);
+  }, tags: 'react-17');
 }
 
 @JS('React.version')
